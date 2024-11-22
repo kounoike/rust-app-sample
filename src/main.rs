ad6fb0db1d7903bf119915cfdff1e61b8cf1ecab
KOUNOIKE Yuusuke <yusuke.konoike@starley.jp>
2024-11-23 02:49:57 +0900

    initial commit（だいぶ書いたけど）

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {}
+
+fn main() {
+    let cli = Cli::parse();
+    println!("CLI:[{:?}]", cli);
+    println!(env!("CARGO_PKG_VERSION"));
+}
